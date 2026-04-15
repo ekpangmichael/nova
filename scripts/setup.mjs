b5725d971a4e3e48361bcafeec9eca28ac0f8286
Michael Ekpang <career.ekpangmichael@gmail.com>
2026-04-16 01:02:52 +0200

    fix setup env loading

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -1,4 +1,4 @@
-import { copyFile } from "node:fs/promises";
+import { copyFile, readFile } from "node:fs/promises";
 import { resolve } from "node:path";
 import { spawn } from "node:child_process";
 
